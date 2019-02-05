69659f369c720bb004266a73edf1cee2144d9097
Denis Semenov <ammofos1996@gmail.com>
2019-02-05 18:03:58 +0300

    This is an exemple of delegation in JAva

---
+++ delegation/src/First/Main.java
@@ -0,0 +1,74 @@
+package First;
+import java.util.Scanner;
+
+
+/*
+Эта программа представляет собой пример использования делегирования с помощью интерфейса
+ */
+
+interface Cook{
+
+    void cooking();
+}
+
+class Chicken implements Cook{
+
+    @Override
+    public void cooking() {
+        System.out.println("Cooking Chicken!");
+    }
+}
+
+class Eggs implements Cook{
+    public void cooking(){
+        System.out.println("Cooking Eggs!") ;
+    }
+}
+
+class Main {
+
+
+    Cook action;
+
+
+    void run() {
+        if (startCooking()) {
+            action.cooking();
+        }
+    }
+
+
+    /*
+    Этот метод в данной программе не важен, он должен представлять
+    собой какое-либо действие, в этом примере, что-то вроде, включить плиту, подготовить сковородку и тд.
+     */
+
+    private boolean startCooking() {
+        return true;
+    }
+
+
+
+
+    public static void main(String[] args) {
+
+        System.out.println("What do you want to eat today?");
+
+        String sc = new Scanner(System.in).nextLine();
+
+
+
+        Main cook = new Main();
+
+
+        if (sc.equals("chicken")) {
+            cook.action = new Chicken();
+        } else if (sc.equals("eggs")) {
+            cook.action = new Eggs();
+        }
+
+
+        cook.run();
+
+    }
+}
